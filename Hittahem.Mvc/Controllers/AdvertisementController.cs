887cb30eb57f807762612374df98733ac8eaf9ae
hannaagerlo <hannaagerlo@gmail.com>
2022-04-08 13:33:09 +0200

    scaffoldat

---
+++ Hittahem.Mvc/Controllers/AdvertisementController.cs
@@ -0,0 +1,160 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hittahem.Mvc.Data;
+using Hittahem.Mvc.Models;
+
+namespace Hittahem.Mvc.Controllers
+{
+    public class AdvertisementController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdvertisementController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Advertisement
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Homes.ToListAsync());
+        }
+
+        // GET: Advertisement/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var home = await _context.Homes
+                .Include(h => h.Agent)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (home == null)
+            {
+                return NotFound();
+            }
+
+            return View(home);
+        }
+
+        // GET: Advertisement/Create
+        public IActionResult Create()
+        {
+            ViewData["AgentId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Advertisement/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Price,Description,Rooms,LivingArea,UninhabitableArea,GardenArea,BuildYear,TimePosted,Adress,HousingType,OwnershipType,Image,AgentId")] Home home)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(home);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AgentId"] = new SelectList(_context.Users, "Id", "Id", home.AgentId);
+            return View(home);
+        }
+
+        // GET: Advertisement/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var home = await _context.Homes.FindAsync(id);
+            if (home == null)
+            {
+                return NotFound();
+            }
+            ViewData["AgentId"] = new SelectList(_context.Users, "Id", "Id", home.AgentId);
+            return View(home);
+        }
+
+        // POST: Advertisement/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Price,Description,Rooms,LivingArea,UninhabitableArea,GardenArea,BuildYear,TimePosted,Adress,HousingType,OwnershipType,Image,AgentId")] Home home)
+        {
+            if (id != home.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(home);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HomeExists(home.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AgentId"] = new SelectList(_context.Users, "Id", "Id", home.AgentId);
+            return View(home);
+        }
+
+        // GET: Advertisement/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var home = await _context.Homes
+                .Include(h => h.Agent)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (home == null)
+            {
+                return NotFound();
+            }
+
+            return View(home);
+        }
+
+        // POST: Advertisement/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var home = await _context.Homes.FindAsync(id);
+            _context.Homes.Remove(home);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HomeExists(int id)
+        {
+            return _context.Homes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Hittahem.Mvc/Models/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using Hittahem.Mvc.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hittahem.Mvc.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                //if(context.Homes.Any())
+                //{
+                //    return;
+                //}
+                context.Homes.AddRange(
+                    new Home
+                    {
+                        Price = 10000000,
+                        Description = "Fint och trevligt hem",
+                        Rooms = 3,
+                        LivingArea = 55,
+                        UninhabitableArea = 23,
+                        GardenArea = 1230,
+                        BuildYear = 1952,
+                        Adress = "Stockholmsvägen 3",
+                        HousingType = Enums.HousingType.Villa,
+                        OwnershipType = Enums.OwnershipType.Bostadsrätt,
+                        Image = "/images/husbild.jpg"
+
+
+                    }) ;
+                context.SaveChanges();
+            }
+        }
+    }
+}
