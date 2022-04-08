49f6a1f5f4acb072ea2171078a062fd2736fcc48
Charlotta Lindberg <clotta_lindberg@hotmail.com>
2022-04-08 12:54:36 +0200

    Migrationer till lottas databas

---
+++ Hittahem.Mvc/Migrations/20220408100704_SecondMigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hittahem.Mvc.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "HomeViewings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 4, 8, 12, 7, 4, 395, DateTimeKind.Local).AddTicks(3298));
+
+            migrationBuilder.UpdateData(
+                table: "Homes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "TimePosted",
+                value: new DateTime(2022, 4, 8, 10, 7, 4, 395, DateTimeKind.Utc).AddTicks(3284));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "HomeViewings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 4, 8, 11, 26, 16, 346, DateTimeKind.Local).AddTicks(588));
+
+            migrationBuilder.UpdateData(
+                table: "Homes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "TimePosted",
+                value: new DateTime(2022, 4, 8, 9, 26, 16, 346, DateTimeKind.Utc).AddTicks(564));
+        }
+    }
+}
