38885608e9b6545771b308267deb3c11273b1ee7
davidsjoblom <davidsjoblom1@hotmail.com>
2022-04-08 11:56:03 +0200

    fixade lite build errors

--- Hittahem.Mvc/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ Hittahem.Mvc/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -11,8 +11,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Hittahem.Mvc.Models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace Hittahem.Mvc.Areas.Identity.Pages.Account
