b7fa537843e6afde9ff5c303c3e7d5f1f2e0bdaa
davidsjoblom <davidsjoblom1@hotmail.com>
2022-04-20 15:45:21 +0200

    scaffoldat RealtorController till mäklargränssnittet, kommenterat ut en rad ut home/index som krävde minst 30 tecken i Description som skapade error (får åtgärdas senare)

---
+++ Hittahem.Mvc/Controllers/RealtorController.cs
@@ -0,0 +1,161 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hittahem.Mvc.Data;
+using Hittahem.Mvc.Models;
+
+namespace Hittahem.Mvc.Controllers
+{
+    public class RealtorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RealtorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Realtor
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Homes.Include(h => h.Agent);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Realtor/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var home = await _context.Homes
+                .Include(h => h.Agent)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (home == null)
+            {
+                return NotFound();
+            }
+
+            return View(home);
+        }
+
+        // GET: Realtor/Create
+        public IActionResult Create()
+        {
+            ViewData["AgentId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Realtor/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Price,Description,Rooms,LivingArea,UninhabitableArea,GardenArea,BuildYear,TimePosted,Adress,HousingType,OwnershipType,Image,AgentId")] Home home)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(home);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AgentId"] = new SelectList(_context.Users, "Id", "Id", home.AgentId);
+            return View(home);
+        }
+
+        // GET: Realtor/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var home = await _context.Homes.FindAsync(id);
+            if (home == null)
+            {
+                return NotFound();
+            }
+            ViewData["AgentId"] = new SelectList(_context.Users, "Id", "Id", home.AgentId);
+            return View(home);
+        }
+
+        // POST: Realtor/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Price,Description,Rooms,LivingArea,UninhabitableArea,GardenArea,BuildYear,TimePosted,Adress,HousingType,OwnershipType,Image,AgentId")] Home home)
+        {
+            if (id != home.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(home);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HomeExists(home.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AgentId"] = new SelectList(_context.Users, "Id", "Id", home.AgentId);
+            return View(home);
+        }
+
+        // GET: Realtor/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var home = await _context.Homes
+                .Include(h => h.Agent)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (home == null)
+            {
+                return NotFound();
+            }
+
+            return View(home);
+        }
+
+        // POST: Realtor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var home = await _context.Homes.FindAsync(id);
+            _context.Homes.Remove(home);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HomeExists(int id)
+        {
+            return _context.Homes.Any(e => e.Id == id);
+        }
+    }
+}
